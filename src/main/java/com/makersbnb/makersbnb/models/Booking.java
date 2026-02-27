398148e54a6fd7070151354a0f9b2759d29d1c11
Richard <rj.n@live.com>
2026-02-27 12:55:34 +0000

    added bookings private fields matching booking in psql

--- src/main/java/com/makersbnb/makersbnb/models/Booking.java
+++ src/main/java/com/makersbnb/makersbnb/models/Booking.java
@@ -1,4 +1,10 @@
 package com.makersbnb.makersbnb.models;
 
+import java.time.LocalDate;
+
 public class Booking {
+    private Integer id;
+    private Integer spaceId;
+    private LocalDate date;
+    private String Status;
 }
