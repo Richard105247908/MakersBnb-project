c397caea7d70e9010a113cf06efc31277cf4098d
Richard <rj.n@live.com>
2026-02-27 13:49:21 +0000

    method for space repositories

--- src/main/java/com/makersbnb/makersbnb/repositories/SpaceRepository.java
+++ src/main/java/com/makersbnb/makersbnb/repositories/SpaceRepository.java
@@ -1,4 +1,20 @@
 package com.makersbnb.makersbnb.repositories;
 
+
+import com.makersbnb.makersbnb.DatabaseConnection;
+import com.makersbnb.makersbnb.models.Space;
+import org.postgresql.ds.PGSimpleDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public class SpaceRepository {
-}
+   public List<Space> findAll() throws SQLException {
+        DatabaseConnection.connect();
+
+       return List.of();
+   }
+    }
+
+
